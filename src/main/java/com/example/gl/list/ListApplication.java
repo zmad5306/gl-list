bb2ec8514ecc23055377f2b42167a79efe17de68
zmad5306 <zachary.maddox@gmail.com>
2018-03-20 17:23:42 -0400

    init project

---
+++ src/main/java/com/example/gl/list/ListApplication.java
@@ -0,0 +1,12 @@
+package com.example.gl.list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListApplication.class, args);
+	}
+}
